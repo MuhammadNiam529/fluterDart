efeafe2698eb5534db18203db0e24559c728e3f4
MuhammadNiam529 <niamulmujib@gmail.com>
2024-01-14 02:20:49 +0700

    backButton

--- lib/pages/base_widgets/button/back_button.dart
+++ lib/pages/base_widgets/button/back_button.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class BackButton extends StatelessWidget {
   final Color color;
 
-  const BackButton(this.color, {Key? key}) : super(key: key);
+  const BackButton(this.color, {super.key});
 
   @override
   Widget build(BuildContext context) {
